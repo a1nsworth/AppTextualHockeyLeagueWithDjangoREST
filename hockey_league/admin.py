f14480e55f47985087f4bf0fe448eba99ab65dcb
a1nsworth <xed225@gmail.com>
2023-11-26 00:23:22 +0300

    register models

--- hockey_league/admin.py
+++ hockey_league/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import HockeyLeague, Season, Team, Player, PlayerStatistic, Ticket, TypeTicket, CashMachine, Match
 
-# Register your models here.
+admin.site.register(HockeyLeague)
+admin.site.register(Season)
+admin.site.register(Team)
+admin.site.register(Player)
+admin.site.register(PlayerStatistic)
+admin.site.register(Match)
+admin.site.register(CashMachine)
+admin.site.register(Ticket)
+admin.site.register(TypeTicket)
